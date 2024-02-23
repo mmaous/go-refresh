402b8d70e148a043dac95ec9ad0d823cc2d8f159
Mhand M <mhandmaous@gmail.com>
2024-02-23 11:40:22 +0100

    Chapter 1.3: Functions, And its characters

---
+++ basics/functions.go
@@ -0,0 +1,17 @@
+package basics
+
+import "fmt"
+
+/*
+- Go functions accept zero or more parameters.
+- Parameters must be explicitly typed, there is no type inference.
+- Values are returned from functions using the return keyword. (Values with s as it can be more than one value)
+*/
+
+func Hey() {
+	fmt.Println("Hey!!")
+}
+
+func hello() {
+	fmt.Println("Hello!!")
+}
