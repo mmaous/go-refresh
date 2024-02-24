7837332e0bd60265992926d2b82300237834bd8d
Mhand M <mhandmaous@gmail.com>
2024-02-24 19:34:39 +0100

    fix: comment unused code

--- basics/operators.go
+++ basics/operators.go
@@ -7,13 +7,12 @@ but in Go this gives an error. (see (1))
 - Shorthand Assignments & Increment and Decrement is the same as most programming language.
 */
 
-
 // (1)
 
 var x int = 42
 
 // this line produces an error
-value := float32(2.0) * x // invalid operation: mismatched types float32 and int
+// value := float32(2.0) * x // invalid operation: mismatched types float32 and int
 
 // you must convert int type to float32 before performing arithmetic operation
-value := float32(2.0) * float32(x)
+// value := float32(2.0) * float32(x)
--- basics/variables.go
+++ basics/variables.go
@@ -1,4 +1,5 @@
 package basics
+
 // NOTE: this file can't be executed
 /*
 - Go is statically-typed, which means all variables must have a defined type at compile-time.
@@ -7,16 +8,15 @@ package basics
 // Variables can be defined by explicitly specifying a type:
 var bathrooms int // Explicitly typed
 
-
 // You can use an initializer, and the compiler will automatically assign the variable type based on the initializer.
-bedrooms := 2
+// bedrooms := 2
 
 // U can always later on update its value
-bedrooms = 3
-bathrooms = 2
+// bedrooms = 3
+// bathrooms = 2
 
 // keep in mind, That u can only pass the value of its initial type
-bathrooms = false // throws error at compile time as its expecting 'int'
+// bathrooms = false // throws error at compile time as its expecting 'int'
 
 // CONSTANT
 
